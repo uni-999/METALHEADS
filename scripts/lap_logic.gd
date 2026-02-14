bb0af820f1dc725cfeb151cff8353f5322a0db3e
Ramil Sharipov <ramilkasharipov@gmail.com>
2026-02-14 13:18:33 +0300

    Made a checkpoint system

--- scripts/lap_logic.gd
+++ scripts/lap_logic.gd
@@ -2,15 +2,48 @@ extends Node2D
 
 @onready var checkpoints_node = $Checkpoints
 @onready var racers_node = $Racers
+@onready var player = $"Racers/Test Player"
+@onready var laptime = $"Lap time"
+@onready var lapcount = $"Lap count"
+
 var checkpoints: Array[Node] = []
 var racers: Array[Node] = []
+var tracking: Dictionary = {}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	checkpoints = checkpoints_node.get_children()
 	racers = racers_node.get_children()
 
+	for r in racers:
+		tracking.get_or_add(r.name, [0, 0, 0])
+	print('Level loaded')
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	lapcount.text = 'Lap count: ' + str(player.lap_count)
+
+
+func _on_sector_1_body_entered(body: Node2D) -> void:
+	print(body, ' has passed Sector 1')
+	if body in racers:
+		tracking[body.name][0] += 1
+	print(tracking[body.name])
+
+func _on_sector_2_body_entered(body: Node2D) -> void:
+	print(body, ' has passed Sector 2')
+	if body in racers:
+		tracking[body.name][1] += 1
+	print(tracking[body.name])
+
+func _on_sector_3_body_entered(body: Node2D) -> void:
+	print(body, ' has passed Sector 3')
+	var checks = tracking[body.name]
+	if body in racers:
+		checks[2] += 1
+	print(checks)
+	if body.lap_count < checks.min():
+		body.lap_count += 1
+	print(body.lap_count)
--- scripts/test_player.gd
+++ scripts/test_player.gd
@@ -1,14 +1,17 @@
-extends Node2D
+extends CharacterBody2D
 
 
-var lap_count = 0
+@export var lap_count = 0
+const SPEED = 300.0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	move_and_slide()
